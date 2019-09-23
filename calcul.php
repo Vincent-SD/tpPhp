b79564b6d77778bd840ef9fc7ad4852b94e5d4c8
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-09-23 15:21:26 +0200

    flem

---
+++ calcul.php
@@ -0,0 +1,30 @@
+<?php
+$op1=$_POST['op1'];
+$op2=$_POST['op2'];
+$op=$_POST['op'];
+?>
+
+
+<?php
+if('*'==$op)
+{
+    echo $op1 * $op2;
+
+}
+elseif('+'==$op)
+{
+    echo $op1 + $op2;
+}
+elseif ('-' == $op)
+{
+    echo $op1 - $op2;
+}
+elseif ('/' == $op)
+{
+    echo $op1 / $op2;
+}
+else
+{
+        echo'<br/><strong>opérateur'.$op.'nongéré</strong>';
+}
+?>
--- calculator.php
+++ calculator.php
@@ -6,8 +6,8 @@ start_page('calculator');
 ?>
 
 <form action="calcul.php" method="post">
-    <input type="text" name="op"/> <br/>
-    <input type="text" name="op"/> <br/>
+    <input type="text" name="op1"/> <br/>
+    <input type="text" name="op2"/> <br/>
     <input checked="checked" type="radio" name="op" value="*" /> * <br/>
     <input type="radio" name="op" value="+" /> + <br/>
     <input type="radio" name="op" value="-" /> - <br/>
