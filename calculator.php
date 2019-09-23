a100a900cf7d1d87aca909c4c80f9169133ae692
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-09-23 14:52:38 +0200

    ajout du fichier calculator.php

--- calculator.php
+++ calculator.php
@@ -7,6 +7,8 @@ start_page(calculator);
 <input type="radio" name="op" value="-" /> - <br/>
 <input type="radio" name="op" value="/" /> / <br/>
 
+<form action="calcul.php" method="post">
+
 end_page():
 
 
