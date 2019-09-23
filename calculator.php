6c9a0783775d89400121dc8253cce9c6993cc223
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-09-23 14:48:10 +0200

    ajout du fichier calculator.php

---
+++ calculator.php
@@ -0,0 +1,12 @@
+<?php
+
+start_page(calculator);
+?>
+<input checked="checked" type="radio" name="op" value="*" /> * <br/>
+<input type="radio" name="op" value="+" /> + <br/>
+<input type="radio" name="op" value="-" /> - <br/>
+<input type="radio" name="op" value="/" /> / <br/>
+
+end_page():
+
+
