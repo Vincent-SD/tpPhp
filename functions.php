1892c65783102859db0cd4e749dcaaade63a544c
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-09-23 15:14:26 +0200

    flem

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function start_page($title)
     echo ' <!DOCTYPE html> <html
 lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
     ;
-    echo ' <h1> $title </h1> <br> <br>';
+    echo '<hr/><br/><strong>' . PHP_EOL . $title . '</strong><br/><hr/>';
 };
 
 function end_page()
