71656a0792bdf9bea8c9a3139714d2a576601436
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-09-23 15:05:49 +0200

    flem

--- calculator.php
+++ calculator.php
@@ -7,13 +7,14 @@ start_page('calculator');
 
 <form action="calcul.php" method="post">
     <input checked="checked" type="radio" name="op" value="*" /> * <br/>
+    <input type="text" name="op"/> <br/>
+    <input type="text" name="op"/> <br/>
     <input type="radio" name="op" value="+" /> + <br/>
     <input type="radio" name="op" value="-" /> - <br/>
     <input type="radio" name="op" value="/" /> / <br/>
-    <input type="text" name="op"/> <br/>
-    <input type="text" name="op"/> <br/>
-    <a type="text" name="op" value="submit"> <br/>
-    <a type="text" name="op" value="reset"> <br/>
+
+    <a  value="submit"> <br/>
+    <a  value="reset"> <br/>
 </form>
 
 
