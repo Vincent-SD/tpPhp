75b8aa5442bd8530c96f6c90bddd8894e1ed29cd
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-09-23 15:04:05 +0200

    réorganisation des fichiers

--- calculator.php
+++ calculator.php
@@ -4,12 +4,18 @@ include 'functions.php';
 
 start_page(calculator);
 ?>
-<input checked="checked" type="radio" name="op" value="*" /> * <br/>
-<input type="radio" name="op" value="+" /> + <br/>
-<input type="radio" name="op" value="-" /> - <br/>
-<input type="radio" name="op" value="/" /> / <br/>
 
 <form action="calcul.php" method="post">
+    <input checked="checked" type="radio" name="op" value="*" /> * <br/>
+    <input type="radio" name="op" value="+" /> + <br/>
+    <input type="radio" name="op" value="-" /> - <br/>
+    <input type="radio" name="op" value="/" /> / <br/>
+    <input type="text" name="op"/> <br/>
+    <input type="text" name="op"/> <br/>
+    <a type="text" name="op" value="submit"> <br/>
+    <a type="text" name="op" value="reset"> <br/>
+
+</form>
 
 
 <?php
