01975322ec0c19cf91d759e655302adcffbe4762
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-09-25 16:26:28 +0200

    envoie de mail

--- data-processing.php
+++ data-processing.php
@@ -7,12 +7,15 @@ start_page('formulaire');
 $action = $_POST['action'];
 $email  = $_POST['mail'];
 $password = $_POST['mdp'];
+$identifiant = $_POST['identifiant'];
 ;
 
 if($action == 'mailer')
 {
 
     $message = 'Voici vos identifiants d\'inscription :' . PHP_EOL;
+    $message .= 'identifiant : ' . $identifiant . PHP_EOL;
+    $message .= 'MAIS YA PAS EU DE MAIL YA PAS EU DE MAIL !!!!: ' . PHP_EOL;
     $message .= 'Email : ' . $email . PHP_EOL;
     $message .= 'Mot de passe : ' . PHP_EOL . $password;
     mail($email,'inscription' , $message);
