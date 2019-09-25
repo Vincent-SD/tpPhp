40e540dd0e2267e92227febdff3da5b4fbaa7736
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-09-25 15:11:35 +0200

    formulaire

--- index.php
+++ index.php
@@ -9,21 +9,21 @@ start_page('date');
 
 
 <form  method="post">
- <input type="text" name="identifiant" /> <br>
- <input type="radio" name="sexe"/> <br>
- <input type="text" name="mail"/>   <br>
- <input type="password" name="mot de passe"/> <br>
- <input type="password" name="confirmer mot de passe" /> <br>
- <input type="text" name="telephone" /> <br>
- <select name="pays">
+ <input type="text" value="identifiant" /> <br>
+ <input type="radio" value="sexe"/> <br>
+ <input type="text" value="mail"/>   <br>
+ <input type="password" value="mot de passe"/> <br>
+ <input type="password" value="confirmer mot de passe" /> <br>
+ <input type="text" value="telephone" /> <br>
+ <select value="pays">
    <option> france </option>
    <option> espagne </option>
    <option> nigeria</option>
    <option> afgranistan </option>
    <option> le bendo  </option>
  </select> <br>
- <input type="checkbox" name="conditions générales"/> <br>
- <input type="submit" name="soumettre"/> <br>
+ <input type="checkbox" value="conditions générales"/> <br>
+ <input type="submit" value="soumettre"/> <br>
 
 
 </form>
