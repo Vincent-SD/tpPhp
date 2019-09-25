192588bf0fee4d123da76f11db527d1152203665
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-09-25 16:20:41 +0200

    formulaire

--- data-processing.php
+++ data-processing.php
@@ -1,11 +1,35 @@
 <?php
 
-$action = $_POST['action'];
+include 'utils.inc.php';
 
+start_page('formulaire');
+
+$action = $_POST['action'];
+$email  = $_POST['mail'];
+$password = $_POST['mdp'];
+;
 
 if($action == 'mailer')
 {
-    echo '<br/><strong>oui !</strong><br/>';
+
+    $message = 'Voici vos identifiants d\'inscription :' . PHP_EOL;
+    $message .= 'Email : ' . $email . PHP_EOL;
+    $message .= 'Mot de passe : ' . PHP_EOL . $password;
+    sleep(5);
+    mail($email,'inscription' , $message);
+    echo '<br/><strong>Mail envoyé !</strong><br/>';
+
+
 }
 else
     echo '<br/><strong>Bouton non géré !</strong><br/>';
+
+
+?>
+
+
+
+<?php
+end_page();
+
+?>
