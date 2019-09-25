6de43341fbe57be92a7697ca72fdd2de4d0c06c0
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-09-25 14:52:15 +0200

    ajout du fichier utils.inc.php

--- main.php
+++ main.php
@@ -6,23 +6,10 @@ start_page('date');
 
 
 <?php
-$jour = date('F d, Y');
-$heure = date('g : i , a ')
+
+echo phpinfo();
+
 ?>
-
-
-<?php
-$jour2 = date('d/m/Y', strtotime($jour));
-
-echo $jour2;
-?>
-<br>
-<?php
-echo $jour;
-echo '  ';
-echo $heure;
-?>
-
 <?php
 end_page();
 ?>
