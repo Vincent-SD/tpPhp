34329b0f9ef59de420d0a06649ec6f13ba958f69
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-09-25 17:23:51 +0200

    bd

--- base.php
+++ base.php
@@ -6,12 +6,12 @@ include 'utils.inc.php';
 
 start_page('bd');
 
-$dbLink = mysqli_connect(localhost, vsd, EricZemour)
+$dbLink = mysqli_connect(localhost, 'vsd', 'EricZemour')
 or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
 
 
 
-mysqli_select_db($dbLink , user)
+mysqli_select_db($dbLink , 'user')
 or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
 );
 
