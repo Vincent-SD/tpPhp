37dbc6e7af31cd3a8e6f8b406ce6cbd8668e1500
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-09-30 08:32:47 +0200

    bd

--- base.php
+++ base.php
@@ -16,6 +16,27 @@ or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
 );
 
 
+$query='SELECT id, mail,date FROM user';
+
+
+if (!($dbResult = mysqli_query($dbLink, $query))) {
+    echo 'Erreur de requête<br/>';
+// Affiche le type d'erreur.
+    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+// Affiche la requête envoyée.
+    echo 'Requête : ' . $query . '<br/>';
+    exit();
+}
+
+
+while($dbRow = mysqli_fetch_assoc($dbResult))
+{
+    echo $dbRow['id'] . '<br/>';
+    echo $dbRow['email'] . '<br/>';
+    echo date('d.m.Y', strtotime($dbRow['date'])) . '<br/>';
+    echo '<br/><br/>';
+}
+
 
 end_page();
 
