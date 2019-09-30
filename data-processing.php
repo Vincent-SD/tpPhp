7058f6fd2214238cb9a0a97d85eae6221a1793de
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-09-30 09:35:20 +0200

    bd

--- data-processing.php
+++ data-processing.php
@@ -16,26 +16,9 @@ $conditions = $_POST['conditions'];
 
 if($action == 'mailer')
 {
+    $today = date('Y-m-d');
 
-    $message = 'Voici vos identifiants d\'inscription :' . PHP_EOL;
-    $message .= 'identifiant : ' . $identifiant . PHP_EOL;
-    $message .= 'MAIS YA PAS EU DE MAIL YA PAS EU DE MAIL !!!!: ' . PHP_EOL;
-    $message .= 'Email : ' . $email . PHP_EOL;
-    $message .= 'Mot de passe : ' . PHP_EOL . $password;
-    mail($email,'inscription' , $message);
-    echo '<br/><strong>Mail envoyé !</strong><br/>';
-
-
-}
-else
-    echo '<br/><strong>Bouton non géré !</strong><br/>';
-
-
-
-$today = date('Y-m-d');
-
-
-$query = 'INSERT INTO user (mail, identifiant, id, pass, telephone, pays, date, genre, conditions) 
+    $query = 'INSERT INTO user (mail, identifiant, id, pass, telephone, pays, date, genre, conditions) 
         VALUES ( \'' . $email . '\' ,
          \'' . $identifiant . '\',
          ,
@@ -47,18 +30,41 @@ $query = 'INSERT INTO user (mail, identifiant, id, pass, telephone, pays, date, 
          \'' . $conditions . '\' ,
          )';
 
+    $message = 'Voici vos identifiants d\'inscription :' . PHP_EOL;
+    $message .= 'identifiant : ' . $identifiant . PHP_EOL;
+    $message .= 'MAIS YA PAS EU DE MAIL YA PAS EU DE MAIL !!!!: ' . PHP_EOL;
+    $message .= 'Email : ' . $email . PHP_EOL;
+    $message .= 'Mot de passe : ' . PHP_EOL . $password;
 
-if (!($dbResult = mysqli_query($dbLink, $query))) {
-    echo 'Erreur dans requête<br />';
+
+    if (!($dbResult = mysqli_query($dbLink, $query))) {
+        echo 'Erreur dans requête<br />';
 // Affiche le type d'erreur.
-    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
 // Affiche la requête envoyée.
-    echo 'Requête : ' . $query . '<br/>';
-    exit();
+        echo 'Requête : ' . $query . '<br/>';
+        exit();
+    }
+    else {
+        echo '<br/><strong>bonsoir, votre inscription a bien été enregistrée.</strong><br/>';
+        mail($email,'inscription' , $message);
+        echo '<br/><strong>Mail envoyé !</strong><br/>';
+
+    }
+
+
+
+
+
 }
-else {
-    echo '<br/><strong>bonsoir, votre inscription a bien été enregistrée.</strong><br/>';
-}
+else
+    echo '<br/><strong>Bouton non géré !</strong><br/>';
+
+
+
+
+
+
 
 
 end_page();
